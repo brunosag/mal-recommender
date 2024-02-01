9bad331a7844c13a4412a668a4c23fba27bcfdfc
Pedro Lubaszewski Lima <74931964+PLLima@users.noreply.github.com>
2024-02-01 13:20:23 -0300

    style: change user's anime_list mongodb functions' variables' names

--- app/api/db/users/anime_lists/route.js
+++ app/api/db/users/anime_lists/route.js
@@ -1,16 +1,18 @@
-import clientPromise from "@/lib/db/mongodb";
+import clientPromise from '@/lib/db/mongodb';
 
 export async function PATCH(request) {
   const params = new URLSearchParams(new URL(request.url).search);
-  const id = params.get("id");
-  const animeList = await request.json();
+  const user_id = params.get('user_id');
 
   const client = await clientPromise;
-  const db = client.db("db");
-  const filter = { _id: parseInt(id, 10) };
+  const db = client.db('db');
+
+  const animeList = await request.json();
+
+  const filter = { _id: parseInt(user_id, 10) };
   const update = { $set: { anime_list: animeList } };
 
-  const result = await db.collection("users").findOneAndUpdate(filter, update);
+  const result = await db.collection('users').findOneAndUpdate(filter, update);
 
   return Response.json(result);
 }
