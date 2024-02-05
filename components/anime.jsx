b991fd9af74d97dbeb19f8118963852b4d2efd18
Bruno Samuel <brunosag02@gmail.com>
2024-02-05 23:22:41 +0000

    fix: extra whitespace after long titles

--- components/anime.jsx
+++ components/anime.jsx
@@ -11,21 +11,12 @@ export default function Anime({ anime, points, relatedAnime, ...props }) {
     <div className="rounded-[1.75rem] flex justify-between p-5 bg-black/[0.15] gap-5" {...props}>
       <div className="flex gap-5">
         <Link href={MALUrl} target="_blank">
-          <div className="relative">
+          <div className="relative w-28 h-40">
+            <Image src={anime.image} alt={anime.title.jp} fill className="rounded-2xl" />
             <Image
               src={anime.image}
               alt={anime.title.jp}
-              width="0"
-              height="0"
-              sizes="100vw"
-              className="w-28 h-auto rounded-2xl"
-            />
-            <Image
-              src={anime.image}
-              alt={anime.title.jp}
-              width="0"
-              height="0"
-              sizes="100vw"
+              fill
               className="w-28 h-auto rounded-2xl absolute top-0 left-0 blur-xl opacity-30 -z-10"
             />
           </div>
@@ -33,13 +24,13 @@ export default function Anime({ anime, points, relatedAnime, ...props }) {
         <div className="flex flex-col justify-center gap-7">
           <div className="font-semibold">
             <Link href={MALUrl} target="_blank" className="inline-block">
-              <div className="flex flex-col gap-1 max-w-[70ch]">
-                <div className="flex items-center gap-3">
-                  <span className="text-2xl/[1]">{anime.title.jp}</span>
+              <div className="flex flex-col gap-1">
+                <div className="flex items-center gap-3 max-w-[70ch]">
+                  <span className="text-2xl/[1] truncate">{anime.title.jp}</span>
                   <div className="rounded-full bg-white/5 px-2 py-1 text-xs font-medium">{anime.mean.toFixed(2)}</div>
                 </div>
                 {anime.title.jp !== anime.title.en && (
-                  <span className="text-foreground/40 leading-none">{anime.title.en}</span>
+                  <span className="text-foreground/40 leading-none truncate">{anime.title.en}</span>
                 )}
               </div>
             </Link>
@@ -83,7 +74,7 @@ export default function Anime({ anime, points, relatedAnime, ...props }) {
             </div>
           </PopoverContent>
         </Popover>
-        <div className="flex flex-col items-center justify-center mr-5">
+        <div className="flex flex-col items-center justify-center px-5">
           <div className="relative">
             <span className="text-4xl/[1] font-extrabold">{Math.round(points)}</span>
             <span className="text-4xl/[1] font-extrabold blur-xl absolute top-0 left-0 opacity-30">
